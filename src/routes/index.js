0980e916122e7ce05da1caaa8325e6b73d7adfa2
mkdorff <mkdorff@outlook.com>
2017-10-11 11:46:13 -0600

    folder filler info, initial routing. No breaking??? :D

---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import createHistory from 'history/createBrowserHistory'
+// import './routes.css'
+
+// import Header from '../components/Header'
+// import Footer from '../components/Footer'
+
+export default function Routes(props) {
+  return (
+    <Router history={createHistory()}>
+      <div className="site-container">
+        {/* <Route path="/:site?" component={Header} /> */}
+        <div className="routes-container">
+          <Switch>
+            <Route path="/" exact render={() => (<div>ROOT ROUTE CONTENT YOOO</div>)} />
+            <Redirect to="/" />
+          </Switch>
+        </div>
+        {/* <Route path='/:site?' component={Footer} /> */}
+      </div>
+    </Router>
+  )
+}
